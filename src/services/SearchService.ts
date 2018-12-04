9fba69f418a586db6d41935003d952c955b2e7be
Michael Zhang <michael13162@gmail.com>
2018-12-03 18:48:16 -0700

    Integrated SearchService.ts with SBOLExplorer (still being mocked).

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -1,33 +1,38 @@
 import device from '../data/searchResults/device_search_results'
 import gfp from '../data/searchResults/gfp_search_results'
 import rbs from '../data/searchResults/rbs_search_results'
+import request = require('request')
 
 export default class SearchService {
   // if mock, loads from the json search results in data file
   static async getComponents(query: string, mock: boolean) {
     let json
-    switch (query) {
-      case 'device':
-        json = device
-        break
-      case 'gfp':
-        json = gfp
-        break
-      case 'rbs':
-        json = rbs
-        break
-      default:
-        json = { hits: [] }
+
+    if (mock) {
+      switch (query) {
+        case 'device':
+          json = device
+          break
+        case 'gfp':
+          json = gfp
+          break
+        case 'rbs':
+          json = rbs
+          break
+        default:
+          json = { hits: [] }
+      }
+    } else {
+      json = await new Promise((resolve, reject) => {
+        request('http://localhost:13162/search?query=' + query, (error, response, body) => {
+          console.log('error:', error)
+          resolve(JSON.parse(body))
+        })
+      })
     }
 
-    if (mock){
-      return json.hits
-             .filter(x => /^https:\/\/synbiohub\.org\/public\/igem/.test(x._source.subject))
-             .slice(0, 10);
-    }
-    else {
-      //TODO request API call to get search result data
-      return {'fully': 'loaded potato'}
-    }
+    return json.hits
+           .filter(x => /^https:\/\/synbiohub\.org\/public\/igem/.test(x._source.subject))
+           .slice(0, 100);
   }
 }
